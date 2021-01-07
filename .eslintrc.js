6c090319ebe0021085ce9157499d289abf36e75d
delhi09 <masataka.okudera.biz@gmail.com>
2021-01-07 21:00:23 +0900

    ESLintのエラー解消

--- .eslintrc.js
+++ .eslintrc.js
@@ -89,6 +89,8 @@ module.exports = {
         explicitSpread: 'ignore',
       },
     ],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'warn',
   },
   overrides: [
     {
