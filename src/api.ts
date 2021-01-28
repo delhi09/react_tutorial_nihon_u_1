71da4a9127676c1518be8752ce5a6fec5ad81938
delhi09 <masataka.okudera.biz@gmail.com>
2021-01-29 00:27:09 +0900

    stringのlistを返す方法がわかった

--- src/api.ts
+++ src/api.ts
@@ -2,11 +2,13 @@ type DogAPIRandomResponse = {
   message: string[];
 };
 
-const fetchImages = async (breed: string): Promise<DogAPIRandomResponse> => {
+const fetchImages = async (breed: string): Promise<string[]> => {
   const response = await fetch(
     `https://dog.ceo/api/breed/${breed}/images/random/12`,
   );
 
-  return (await response.json()) as Promise<DogAPIRandomResponse>;
+  const data: Promise<DogAPIRandomResponse> = (await response.json()) as Promise<DogAPIRandomResponse>;
+
+  return (await data).message;
 };
 export default fetchImages;
