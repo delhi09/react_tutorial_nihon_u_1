338a64178ed5e6fef76cb3337155f07beaa36397
delhi09 <masataka.okudera.biz@gmail.com>
2021-01-30 15:29:17 +0900

    Formを追加

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,30 @@ const Header: FC = () => {
   );
 };
 
+const Form: FC = () => {
+  return (
+    <div>
+      <form>
+        <div className="field has-addons">
+          <div className="control is-expanded">
+            <div className="select is-fullwidth">
+              <select name="breed" defaultValue="shiba">
+                <option value="shiba">Shiba</option>
+                <option value="akita">Akita</option>
+              </select>
+            </div>
+          </div>
+          <div className="control">
+            <button type="submit" className="button is-dark">
+              Reload
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
 type ImageProps = {
   src: string;
 };
@@ -69,6 +93,11 @@ const Main: FC = () => {
 
   return (
     <main>
+      <section className="section">
+        <div className="container">
+          <Form />
+        </div>
+      </section>
       <section className="section">
         <div className="container">
           <Gallery urls={urls} />
